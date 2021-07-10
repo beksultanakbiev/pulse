754afc20186a6967bdead1a918f10b0fe7224ba5
beksultanakbiev <85690654+beksultanakbiev@users.noreply.github.com>
2021-07-11 00:07:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src ="icons/arrow_prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src ="icons/arrow_next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content_active').toggleClass('catalog-item__content');
+            $('.catalog-item__list').toggleClass('catalog-item__list_active');
+        })
+    })
+});
